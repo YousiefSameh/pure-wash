0153370ca1662b7c6200775c41e4d5d17eef7856
Yousief Sameh <yousief.sameh@outlook.com>
2025-08-27 05:22:10 +0300

    feat: Add Subscription section - Implement Card component to use in services section and subscription section - Implement SubscriptionSection component clean, responsive, modern

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import CallToActionSection from "@/components/home/CallToAction";
 import HeroSection from "@/components/home/Hero";
 import ServiceSection from "@/components/home/Services";
+import SubscriptionSection from "@/components/home/Subscription";
 
 export default function HomePage() {
   return (
@@ -8,6 +9,7 @@ export default function HomePage() {
       <HeroSection />
       <ServiceSection />
       <CallToActionSection />
+      <SubscriptionSection />
     </main>
   )
 }
